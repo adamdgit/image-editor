08d7ba2f508de12e80d7d5446e2641e563fb1acf
adamdgit <adam_d@outlook.com>
2024-06-13 17:57:03 +1000

    Updated left toolbar, added hover tooltips

--- app/canvas.js
+++ app/canvas.js
@@ -52,8 +52,10 @@ selected_canvas.addEventListener('pointerdown', handlePointerDown);
 currentLayer.children[0].addEventListener('click', (e) => hide_layer(e));
 currentLayer.children[1].addEventListener('click', (e) => update_selected_layer(e));
 currentLayer.children[2].addEventListener('click', () => {
-  if (!remove_layer(currentLayer.dataset.layerId)) return;
-  add_history_event("delete-layer", selected_canvas);
+  // only add to history if there is a layer to remove, never remove inital canvas layer
+  if (remove_layer(currentLayer.dataset.layerId)) {
+    add_history_event("delete-layer", selected_canvas);
+  }
 });
 add_history_event("canvas-edit", selected_canvas);
 add_history_event("add-new-layer", currentLayer);
@@ -83,50 +85,6 @@ pencilButton.addEventListener('click', (e) => update_selected_tool(e.target));
 eraserButton.addEventListener('click', (e) => update_selected_tool(e.target));
 bucketButton.addEventListener('click', (e) => update_selected_tool(e.target));
 
-
-//----- draw layers on canvas -----//
-// function handleLayers() {
-//   const temp1 = {
-//     topLeft: [0,0],
-//     bottomRight: [2,2],
-//     data: [231,20,38,255, 231,20,38,255, 231,20,38,255, 231,20,38,255, 231,20,38,255, 231,20,38,255, 231,20,38,255, 231,20,38,255, 231,20,38,255]
-//   };
-//   const temp2 = {
-//     topLeft: [1,1],
-//     bottomRight: [3,3],
-//     data: [100,100,100,255, 100,100,100,255, 100,100,100,255, 100,100,100,255, 100,100,100,255, 100,100,100,255, 100,100,100,255, 100,100,100,255, 100,100,100,255]
-//   };
-
-//   const layers_stack = [];
-
-//   const combinedData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-//   const data = combinedData.data;
-
-//   // bottom layer first, top layer last, and build the combined array
-//   // starting at the top (first layer)
-//   layers_stack.push(temp2);
-//   layers_stack.push(temp1);
-
-//   while (layers_stack.length > 0) {
-//     // top layer
-//     let top = layers_stack.pop(); 
-//     let startIndex = top.topLeft[0] * top.topLeft[1] * 4;
-//     // copy the layer data to the combined layer data
-//     for (let j = startIndex; j < data.length; j += 4) {
-//         // every 4 is rgba of a pixel, check if the top.data is transparent
-//         // only add non transparent rgba values to the combined data.
-//         let rgba = [top.data[j], top.data[j +1], top.data[j +2], top.data[j +3]].join("")
-//         if (rgba === '0000') continue; // 0000 = transparent
-
-//         data[j] = top.data[j];
-//         data[j +1] = top.data[j +1];
-//         data[j +2] = top.data[j +2];
-//         data[j +3] = top.data[j +3];
-//     }
-//   }
-//   console.log(combinedData.data)
-//   ctx.putImageData(combinedData, 0, 0)
-// }
 
 // Adds new canvas and layer element with matching id's
 function add_new_layer(canvasData, id) {
